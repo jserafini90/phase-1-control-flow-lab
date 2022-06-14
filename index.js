6f0f65dac8aed8bc487989561afefb6378b85580
Jessica Serafini <jpitts_kiheihs@yahoo.com>
2022-06-14 14:01:25 -0400

    commit tree 31d09e2bce01462ad00dbf63aba7d9faf67447e3

--- index.js
+++ index.js
@@ -1,11 +1,31 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(distance){
+  let response
+  if (distance <= 400){ 
+  response = "This one is on me!"
+return response;}
+else if (distance > 2000 && distance < 2500){
+  response = "I will gladly take your thirty bucks."
+  return response;}
+else if (distance >= 2500){
+  response = "No can do."
+  return response;}
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+function ternaryCheckCity(city){
+  return city === 'NYC'?"Ok, sounds good." :`No go.`
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(tip){
+  let msg;
+  switch(tip){
+    case "generous":
+      msg = "Thank you so much."
+      break;
+      case "not as generous":
+      msg = "Thank you."
+      break;
+      default:
+    msg = `Bye.`
+  }
+  return msg
 }
